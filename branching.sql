04b176e900a77631ff233ec93426becff68aadef
RicardoCalvoP <a01028889@tec.mx>
2024-02-29 14:05:08 -0600

    SQL V1

---
+++ branching.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS act_git_branching;
+CREATE DATABASE act_git_branching;
+
+USE act_git_branching;
+
+CREATE TABLE members(
+
+	MemberID INT NOT NULL,
+	name VARCHAR(20)
+
+);
+
+
